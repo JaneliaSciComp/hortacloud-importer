a736cebe20549b97e3a9cbc96666a21047170d2c
takashi310 <ymrnztm310@gmail.com>
2022-05-19 12:43:56 -0400

    revert the lsf cluster

--- tiff2octree.py
+++ tiff2octree.py
@@ -51,7 +51,164 @@ from rasterio.windows import Window
 
 from scipy import ndimage
 
-import dask_janelia
+
+dask.config.set({"jobqueue.lsf.use-stdin": True})
+
+threading_env_vars = [
+    "NUM_MKL_THREADS",
+    "OPENBLAS_NUM_THREADS",
+    "OPENMP_NUM_THREADS",
+    "OMP_NUM_THREADS",
+]
+
+def make_single_threaded_env_vars(threads: int) -> List[str]:
+    return [f"export {var}={threads}" for var in threading_env_vars]
+
+
+def bsub_available() -> bool:
+    """Check if the `bsub` shell command is available
+    Returns True if the `bsub` command is available on the path, False otherwise. This is used to check whether code is
+    running on the Janelia Compute Cluster.
+    """
+    result = which("bsub") is not None
+    return result
+
+
+def get_LSFCLuster(
+    threads_per_worker: int = 1,
+    walltime: str = "1:00",
+    death_timeout: str = "600s",
+    **kwargs,
+) -> LSFCluster:
+    """Create a dask_jobqueue.LSFCluster for use on the Janelia Research Campus compute cluster.
+    This function wraps the class dask_jobqueue.LSFCLuster and instantiates this class with some sensible defaults,
+    given how the Janelia cluster is configured.
+    This function will add environment variables that prevent libraries (OPENMP, MKL, BLAS) from running multithreaded routines with parallelism
+    that exceeds the number of requested cores.
+    Additional keyword arguments added to this function will be passed to the dask_jobqueue.LSFCluster constructor.
+    Parameters
+    ----------
+    threads_per_worker: int
+        Number of cores to request from LSF. Directly translated to the `cores` kwarg to LSFCluster.
+    walltime: str
+        The expected lifetime of a worker. Defaults to one hour, i.e. "1:00"
+    cores: int
+        The number of cores to request per worker. Defaults to 1.
+    death_timeout: str
+        The duration for the scheduler to wait for workers before flagging them as dead, e.g. "600s". For jobs with a large number of workers,
+        LSF may take a long time (minutes) to request workers. This timeout value must exceed that duration, otherwise the scheduler will
+        flag these slow-to-arrive workers as unresponsive and kill them.
+    **kwargs:
+        Additional keyword arguments passed to the LSFCluster constructor
+    Examples
+    --------
+    >>> cluster = get_LSFCLuster(cores=2, project="scicompsoft", queue="normal")
+    """
+
+    if "env_extra" not in kwargs:
+        kwargs["env_extra"] = []
+
+    kwargs["env_extra"].extend(make_single_threaded_env_vars(threads_per_worker))
+
+    USER = os.environ["USER"]
+    HOME = os.environ["HOME"]
+
+    if "local_directory" not in kwargs:
+        # The default local scratch directory on the Janelia Cluster
+        kwargs["local_directory"] = f"/scratch/{USER}/"
+
+    if "log_directory" not in kwargs:
+        log_dir = f"{HOME}/.dask_distributed/"
+        Path(log_dir).mkdir(parents=False, exist_ok=True)
+        kwargs["log_directory"] = log_dir
+
+    cluster = LSFCluster(
+        cores=threads_per_worker,
+        walltime=walltime,
+        death_timeout=death_timeout,
+        **kwargs,
+    )
+    return cluster
+
+
+def get_LocalCluster(threads_per_worker: int = 1, n_workers: int = 0, memory_limit: str = '16GB', **kwargs):
+    """
+    Creata a distributed.LocalCluster with defaults that make it more similar to a deployment on the Janelia Compute cluster.
+    This function is a light wrapper around the distributed.LocalCluster constructor.
+    Parameters
+    ----------
+    n_workers: int
+        The number of workers to start the cluster with. This defaults to 0 here.
+    threads_per_worker: int
+        The number of threads to assign to each worker.
+    **kwargs:
+        Additional keyword arguments passed to the LocalCluster constructor
+    Examples
+    --------
+    >>> cluster = get_LocalCluster(threads_per_worker=8)
+    """
+    return LocalCluster(
+        n_workers=n_workers, threads_per_worker=threads_per_worker, memory_limit=memory_limit, **kwargs
+    )
+
+
+def get_cluster(
+    threads_per_worker: int = 1,
+    walltime: str = "1:00",
+    local_memory_limit: str = "16GB",
+    deployment: Optional[str] = None,
+    local_kwargs: Dict[str, Any] = {},
+    lsf_kwargs: Dict[str, Any] = {"memory": "16GB"},
+) -> Union[LSFCluster, LocalCluster]:
+
+    """Convenience function to generate a dask cluster on either a local machine or the compute cluster.
+    Create a distributed.Client object backed by either a dask_jobqueue.LSFCluster (for use on the Janelia Compute Cluster)
+    or a distributed.LocalCluster (for use on a single machine). This function uses the output of the bsubAvailable function
+    to determine whether code is running on the compute cluster or not.
+    Additional keyword arguments given to this function will be forwarded to the constructor for the Client object.
+    Parameters
+    ----------
+    threads_per_worker: int
+        Number of threads per worker. Defaults to 1.
+    deployment: str or None
+        Which deployment (LocalCluster or LSFCluster) to prefer. If deployment=None, then LSFCluster is preferred, but LocalCluster is used if
+        bsub is not available. If deployment='lsf' and bsub is not available, an error is raised.
+    local_kwargs: dict
+        Dictionary of keyword arguments for the distributed.LocalCluster constructor
+    lsf_kwargs: dict
+        Dictionary of keyword arguments for the dask_jobqueue.LSFCluster constructor
+    """
+
+    if "cores" in lsf_kwargs:
+        warnings.warn(
+            "The `cores` kwarg for LSFCLuster has no effect. Use the `threads_per_worker` argument instead."
+        )
+
+    if "threads_per_worker" in local_kwargs:
+        warnings.warn(
+            "the `threads_per_worker` kwarg was found in `local_kwargs`. It will be overwritten with the `threads_per_worker` argument to this function."
+        )
+
+    if deployment is None:
+        if bsub_available():
+            cluster = get_LSFCLuster(threads_per_worker, **lsf_kwargs)
+        else:
+            cluster = get_LocalCluster(threads_per_worker, **local_kwargs)
+    elif deployment == "lsf":
+        if bsub_available():
+            cluster = get_LSFCLuster(threads_per_worker, walltime, **lsf_kwargs)
+        else:
+            raise EnvironmentError(
+                "You requested an LSFCluster but the command `bsub` is not available."
+            )
+    elif deployment == "local":
+        cluster = get_LocalCluster(threads_per_worker, 0, local_memory_limit, **local_kwargs)
+    else:
+        raise ValueError(
+            f'deployment must be one of (None, "lsf", or "local"), not {deployment}'
+        )
+
+    return cluster
 
 # https://stackoverflow.com/questions/62567983/block-reduce-downsample-3d-array-with-mode-function
 def blockify(image, block_size):
@@ -360,19 +517,24 @@ def setup_cluster(
     if cluster_address:
         cluster = cluster_address
     elif is_lsf:
-        cluster = dask_janelia.deploy.get_LSFCLuster(threads_per_worker=1, walltime=walltime, project=project, memory=memory_limit)
+        my_lsf_kwargs={}
+        my_lsf_kwargs['memory'] = memory_limit
+        if project:
+            my_lsf_kwargs['project'] = project
+        cluster = get_cluster(deployment="lsf", walltime=walltime, lsf_kwargs = my_lsf_kwargs)
         cluster.adapt(minimum_jobs=1, maximum_jobs = lsf_maximum_jobs)
         cluster.scale(thread_num)
     else:
-        cluster = LocalCluster(n_workers=1, threads_per_worker=thread_num, memory_limit=memory_limit)
+        cluster = get_cluster(deployment="local", local_memory_limit = memory_limit)
+        cluster.scale(thread_num)
 
-    dashboard_address = ":8787"
-    client = None
+    dashboard_address = None
     if monitoring: 
-        logging.info(f"Starting dashboard on {dashboard_address}")
+        dashboard_address = ":8787"
+        print(f"Starting dashboard on {dashboard_address}")
         client = Client(address=cluster, processes=True, dashboard_address=dashboard_address)
-    elif cluster_address:
-        client = Client(address=cluster, dashboard_address=None)
+    else:
+        client = Client(address=cluster)
 
     return client
 
@@ -515,7 +677,7 @@ def save_transform_txt(
             Path(ktxout).mkdir(parents=True, exist_ok=True)
             copyfile(tr_path, os.path.join(ktxout, "transform.txt"))
 
-def covert_tiff_to_tiled_tiff(input_paths: List[str], task_num: int, outdir: str, maxbatch: int = 200, resume: bool = False):
+def covert_tiff_to_tiled_tiff(input_paths: List[str], task_num: int, outdir: str, maxbatch: int = 100, resume: bool = False):
     slice_files = input_paths
     futures = []
     logging.info("tiff-to-tiled-tiff conversion")
@@ -525,7 +687,9 @@ def covert_tiff_to_tiled_tiff(input_paths: List[str], task_num: int, outdir: str
     if batch_slice_num < 1: 
         batch_slice_num = 1
     chunked_tif_list = np.array_split(slice_files, (int)(len(slice_files) / batch_slice_num))
+    logging.info("tiff-to-tiled-tiff conversion: list " + str(len(chunked_tif_list)))
     for tlist in chunked_tif_list:
+        logging.info(tlist)
         future = dask.delayed(conv_tiled_tiffs)(tlist, outdir, (256, 256), resume)
         futures.append(future)
     with ProgressBar():
@@ -582,6 +746,7 @@ def save_tiff_blocks(input_slices: List[str], output_path: str, z: int, nlevels:
         dask.compute(futures)
 
 def check_tiff_blocks(output_path: str, z: int, nlevels: int, ch: int, darray: da.Array):
+    logging.info("resume: checking tiff blocks")
     futures = []
     bnum = pow(2, nlevels - 1)
     samples_per_pixel = darray.shape[3]
